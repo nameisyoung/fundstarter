2268221eb0198ddc4eb94729cb78ac9cef0f2e78
Ubuntu <ubuntu@ip-172-31-36-64.us-west-2.compute.internal>
2016-09-27 19:48:02 +0000

    make it better

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ var express = require('express')
 var app = express()
 
 
-app.set('port', (process.env.PORT || 8080))
-//app.use(express.static(__dirname + '/public'))
+//app.set('port', (process.env.PORT || 8080))
+app.use(express.static(__dirname + '/public'))
 
 //__dirname returns the directory that the currently executing script is in.
 
+/* serves main page */
 app.get('/', function(request, response) {
-    response.sendFile('public/index.html',{root:__dirname})
+//    response.sendFile('public/index.html',{root:__dirname})
+    reponse.sendFile(__dirname + '/index.html')
 
 /* sends an entire HTTP response to the client,                                                                                                                                     
  including headers and content,                                                                                                                                                     
@@ -17,6 +19,10 @@ app.get('/', function(request, response) {
 
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at :" + app.get('port'))
+var port = process.env.PORT || 8080
+
+//app.listen(app.get('port'), function() {
+//  console.log("Node app is running at :" + app.get('port'))
+app.listen(port, function() {
+    console.log("Node app is running at :" +port)
 })
