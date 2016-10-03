2b2f2d01175e40d262e556e7b845f19ad395a859
Ubuntu <ubuntu@ip-172-31-46-251.us-west-2.compute.internal>
2016-10-03 08:57:20 +0000

    part1 a fs.readFileSync

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ var port = Number(process.env.PORT || 8080);
 
 // web server object
 http.createServer(function (request, response) {
-    fs.readFile('./public/index.html', function (err, html){
+//    fs.readFile('./public/index.html', function (err, html){
 	response.writeHead(200, {'Contnt-Type': 'text/plain'});
-	response.write(html);
+//	response.write(html);
+    response.write(fs.readFileSync('./public/index.html'));
 	response.end();
-    });
+//    });
 }).listen(port);
 
 console.log("Node app is running at:", port);
