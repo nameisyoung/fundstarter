14163410c166023556cd4feccbad704e4f1ed553
Ubuntu <ubuntu@ip-172-31-46-251.us-west-2.compute.internal>
2016-10-03 07:17:52 +0000

    simple fs.readFile

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-var http = require('http');
+var http = require("http");
+var fs = require("fs");
 
 
 // web server object
 http.createServer(function (request, response) {
-    response.writeHead(200, {'Contnt-Type': 'text/plain'
+    fs.readFile('/public/index.html', function (err, data){
+	response.writeHead(200, {'Contnt-Type': 'text/plain'});
+	response.write('Simple Simple');
+	response.end();
     });
-    response.write('Simple Simple');
-    response.end();
 }).listen(8080);
