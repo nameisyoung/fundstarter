ed99b78bc1bf30f97fab6bbc5ef1b984066338a0
Ubuntu <ubuntu@ip-172-31-46-251.us-west-2.compute.internal>
2016-10-03 06:41:35 +0000

    back to simple

--- index.js
+++ index.js
@@ -1,41 +1,10 @@
-//var express = require('express')
-//var app = express()
 var http = require('http');
-var fs = require('fs');
 
 
-// Web server object
-fs.readFileSync('./public/index.html', function(err, html) {
-    if(err) {
-	throw err;
-    }
-    http.createServer(function(request, response) {
-	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.write(html);
-	response.end();
-    }).listen(8080);
-});
-//app.set('port', (process.env.PORT || 8080))
-//app.use(express.static(__dirname + '/public'))
-
-//__dirname returns the directory that the currently executing script is in.
-
-/* serves main page */
-//app.get('/', function(request, response) {
-//    response.sendFile('public/index.html',{root:__dirname})
-//    reponse.sendFile(__dirname + '/index.html')
-
-/* sends an entire HTTP response to the client,                                                                                                                                     
- including headers and content,                                                                                                                                                     
- which is why you can only call once*/
-
-
-//})
-
-//var port = process.env.PORT || 8080
-
-//app.listen(app.get('port'), function() {
-//  console.log("Node app is running at :" + app.get('port'))
-//app.listen(port, function() {
-//    console.log("Node app is running at :" +port)
-//})
+// web server object
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Contnt-Type': 'text/plain'
+    });
+    response.write('Simple Simple');
+    response.end();
+}).listen(8080);
